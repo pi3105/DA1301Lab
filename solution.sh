8ab5c1f43b7e367705f9b7dddc56b8df710daa85
pi3105 <am25b025@smail.iitm.ac.in>
2025-08-26 15:38:57 +0530

    Added solution.sh

---
+++ solution.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sort numbers.txt
+ip addr
+cat readme.txt
+wc -l data.csv
+grep -ilr error logs/
+tail -n10 app.log
+chmod a+x script.sh
+history -20
+ps aux --sort -rss
+find / -type d -name backup
+sed 's/foo/bar/g' example.txt > new_example.txt
