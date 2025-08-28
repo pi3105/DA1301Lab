f7f9fdb774adf4de611cf0038efce948315c9ee2
pi3105 <am25b025@smail.iitm.ac.in>
2025-08-28 07:17:26 +0530

    Update solution.sh
    Changed it to correct stuff

--- solution.sh
+++ solution.sh
@@ -3,10 +3,10 @@ sort numbers.txt
 ipconfig getifaddr en0
 cat readme.txt
 wc -l data.csv
-grep -ilr error logs/
+grep -lr error logs/
 tail -n10 app.log
 chmod a+x script.sh
-grep TODO *.py
+grep -ir TODO *.py
 history -20
 ps aux --sort -rss
 find / -type d -name backup
